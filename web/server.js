30e8bfa2d093322bcb328a3855ba572388dd3973
Renan Duarte <71858987+DuarteRenan@users.noreply.github.com>
2023-09-14 20:24:59 -0300

    Update server.js
    Change Const baseURL

--- web/server.js
+++ web/server.js
@@ -1,6 +1,7 @@
 import axios from "axios"
 
+const baseURL = process.env.NLW_APP_API_URL;
 
 export const server = axios.create({
-    baseURL: "https://nlw-ia-deploy.netlify.app",
+    baseURL,
 })
