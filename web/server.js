5244dbcca9e01cad18a9df03faf54ef98c582652
Renan Duarte <71858987+DuarteRenan@users.noreply.github.com>
2023-09-14 20:28:22 -0300

    Update server.js
    Reupdate server.js

--- web/server.js
+++ web/server.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-const baseURL = process.env.NLW_APP_API_URL;
+const baseURL = process.env.REACT_APP_API_URL || "http://localhost:3333";
 
 export const server = axios.create({
     baseURL,
