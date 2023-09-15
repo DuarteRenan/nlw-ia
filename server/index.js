a9b406b321e816b30b43ef9e089fe6c68a5c9c41
Renan Duarte <71858987+DuarteRenan@users.noreply.github.com>
2023-09-14 21:32:58 -0300

    Update index.js

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ const app = express()
 app.use(express.json())
 app.use(cors())
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'https://nlw-ia-deploy.netlify.app');
+  res.setHeader('Access-Control-Allow-Origin', 'https://main--nlw-ia-deploy.netlify.app');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   next();
