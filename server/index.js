5bfc86a11b75e9db530efb4a97685b287bc07e57
Renan Duarte <71858987+DuarteRenan@users.noreply.github.com>
2023-09-14 21:30:30 -0300

    Update index.js
    change order

--- server/index.js
+++ server/index.js
@@ -8,15 +8,13 @@ import { summarize } from './summarize.js'
 
 const app = express()
 app.use(express.json())
-
+app.use(cors())
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', 'https://nlw-ia-deploy.netlify.app');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   next();
 });
-
-app.use(cors())
 
 app.get('/summary/:id', async (request, response) => {
     try{
